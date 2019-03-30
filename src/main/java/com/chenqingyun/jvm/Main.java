59c6c4af1a706c3657353cc26f33c13a6c1b3366
chenqingyun <ichenqingyun@gmail.com>
2019-03-31 01:35:29 +0800

    Java Visual Machine Runtime Data Areas

---
+++ src/main/java/com/chenqingyun/jvm/Main.java
@@ -0,0 +1,11 @@
+package com.chenqingyun.jvm;
+
+/**
+ * @author chenqingyun
+ * @date 2019/3/30 09:08.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello,world");
+    }
+}
